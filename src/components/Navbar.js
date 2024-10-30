9ac1006e37c79dfaf1bdc2b568335e11d50d2271
Eliana Pereira <98112983+Eli032501@users.noreply.github.com>
2024-10-30 15:31:35 +0000

    menu open and close handled

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
 
 export default function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+  function handleMenu() {
+    setMenuOpen(!menuOpen);
+  }
+
   return (
     <nav className="Navbar">
       <strong>Frontend dev</strong>
-      <div className="menu-icon menu-icon-cl">
+      <div
+        className={`${menuOpen ? "" : "menu-icon-cl"}  menu-icon`}
+        onClick={handleMenu}
+      >
         <span></span>
         <span></span>
         <span></span>
       </div>
-      <div className="menu-group ">
+      <div className={`${menuOpen ? "d-none" : ""}  menu-group`}>
         <span className="menu-bg menu-position">
           <ul>
             <li className="menu-link">
