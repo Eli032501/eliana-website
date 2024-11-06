f4b5abee64f5e87e0960ab8307efe9605984a92e
Eliana Pereira <98112983+Eli032501@users.noreply.github.com>
2024-11-06 12:08:05 +0000

    added icon - tab / fix console error

--- src/components/SubText.js
+++ src/components/SubText.js
@@ -4,7 +4,7 @@ import "./SubText.css";
 export default function SubText({ text, side }) {
   return (
     <div className={`${side} SubText `}>
-      <span class="material-symbols-outlined">switch_access_shortcut</span>
+      <span className="material-symbols-outlined">switch_access_shortcut</span>
       <p>{text}</p>
     </div>
   );
