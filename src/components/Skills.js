e6524bc826b37ff2e72e42c4e97d4c5c989ada10
Eliana Pereira <98112983+Eli032501@users.noreply.github.com>
2024-11-08 19:27:39 +0000

    added icons - new conponent
    fontawesome - icons
    images icons

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,21 +1,20 @@
 import React from "react";
 import "./SocialSection.css";
-import social1 from "../assets/git.png";
+import vs from "../assets/vs.png";
 
 import ImgStyle from "./ImgStyle";
 
 export default function Skills() {
   return (
     <div className="Skills flex-row">
-      <ImgStyle src={social1} alt="my github" styleClass="footer-icon" />
-      <ImgStyle src={social1} alt="my github" styleClass="footer-icon" />
-      <ImgStyle src={social1} alt="my github" styleClass="footer-icon" />
-      <ImgStyle src={social1} alt="my github" styleClass="footer-icon" />
-      <ImgStyle src={social1} alt="my github" styleClass="footer-icon" />
-      <ImgStyle src={social1} alt="my github" styleClass="footer-icon" />
-      <ImgStyle src={social1} alt="my github" styleClass="footer-icon" />
-      <ImgStyle src={social1} alt="my github" styleClass="footer-icon" />
-      <ImgStyle src={social1} alt="my github" styleClass="footer-icon" />
+      <i className="fa-brands fa-html5 fa-2xl"></i>
+      <ImgStyle src={vs} alt="vs" styleClass="footer-icon" />
+      <ImgStyle src={vs} alt="vs" styleClass="footer-icon" />
+      <ImgStyle src={vs} alt="vs" styleClass="footer-icon" />
+      <ImgStyle src={vs} alt="vs" styleClass="footer-icon" />
+      <ImgStyle src={vs} alt="vs" styleClass="footer-icon" />
+      <ImgStyle src={vs} alt="vs" styleClass="footer-icon" />
+      <ImgStyle src={vs} alt="vs" styleClass="footer-icon" />
     </div>
   );
 }
